165378051614f353a5b8d57a479ea522d1c2dead
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-07-13 07:14:28 -0400

    Created html, css, and js files. Set up basic form to collect input

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+var apiKey = "0773ca9aaf5c56e841a981221d072a10";
+var city; // this will be from user input
+var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+//var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=" + part + "&appid=" + apiKey;
