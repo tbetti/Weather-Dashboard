fcd16ab7eaeb0edfc9e7f33820fa637ce87f9fac
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-07-13 09:47:35 -0400

    User input is read. Fetch used to read in lat and lon data for each city

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,43 @@
+var formEl = document.querySelector("#weather-form");
+var cityEl = document.querySelector("#city");
 var apiKey = "0773ca9aaf5c56e841a981221d072a10";
-var city; // this will be from user input
-var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-//var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=" + part + "&appid=" + apiKey;
+var city = ""; // this will be from user input
+var lat;
+var lon;
+
+var formSubmit = function(event){
+    event.preventDefault();
+    
+    city = splitString(cityEl.value);
+    console.log(city);
+    
+    getLatLon();
+    //getWeather();
+}
+
+// Get latitude and longitude of city
+function getLatLon(){
+    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+    fetch(requestUrl)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            lat = data.coord.lat;
+            lon = data.coord.lon;
+            
+            var forecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+            console.log(forecastUrl);
+        });
+}
+
+// Take any city name that has multiple words and join each word with a "+".
+// Example: New York City => New+York+City
+function splitString(string){
+    string = string.split(" ");
+    string = string.join("+");
+    return string;
+}
+
+formEl.addEventListener("submit", formSubmit);
